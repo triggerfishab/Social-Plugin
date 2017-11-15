f5d92a7a94db5d66fba85403debb9ce87fa560e6
Joi Glifberg <joi.glifberg@triggerfish.se>
2017-11-15 11:36:00 +0100

    Remove testfile.

--- -test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$facebook_accounts = Triggerfish\Social\Accounts::get_accounts( 'facebook' );
-
-pr_log( $facebook_accounts );
-
-foreach ( $facebook_accounts as $facebook_account ) {
-	$facebook_account->sync();
-}
