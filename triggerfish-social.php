3cba7686c4c6e23bc992e88140891694916ac847
phpanos <phpanos@gmail.com>
2018-05-07 15:50:58 +0200

    1.6.1

--- triggerfish-social.php
+++ triggerfish-social.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin name: Triggerfish Social
- * Version: 1.6.0
+ * Version: 1.6.1
  * Text Domain: triggerfish-social
  * Author: Triggefish
  * Author URI: https://www.triggerfish.se/
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
-define( 'PLUGIN_VERSION', '1.6.0' );
+define( 'PLUGIN_VERSION', '1.6.1' );
 
 define( 'PLUGIN_DIR', __DIR__ );
 define( 'PLUGIN_FILE', __FILE__ );
