8ac2bc846762afc871b330d110704cec919aaf53
Anton Söderdahl <anton.soderdahl@triggerfish.se>
2018-06-26 12:48:15 +0200

    1.7.2

--- triggerfish-social.php
+++ triggerfish-social.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin name: Triggerfish Social
- * Version: 1.7.1
+ * Version: 1.7.2
  * Text Domain: triggerfish-social
  * Author: Triggefish
  * Author URI: https://www.triggerfish.se/
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
-define( 'PLUGIN_VERSION', '1.7.1' );
+define( 'PLUGIN_VERSION', '1.7.2' );
 
 define( 'PLUGIN_DIR', __DIR__ );
 define( 'PLUGIN_FILE', __FILE__ );
